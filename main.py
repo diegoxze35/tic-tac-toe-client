290f40ab93ad6952c7a5b011e1f35fed8b7d3866
diegoxze35 <diegoalv1217@gmail.com>
2025-04-20 21:54:05 -0600

    New messages to handle multiplayer states has been added

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from os import system, name as os_name
 def cls():
     system('cls' if os_name == 'nt' else 'clear')
 
+
 def update_board(b: list[list[str]], coordinates: tuple[int, int] = None, new_square: str = None) -> None:
     """
     Función que actualiza el tablero en cada turno
@@ -68,78 +69,89 @@ if __name__ == '__main__':
         # Creamos un texto de consola con el título del juego
         title = figlet_format(text='Tic Tac Toe!', font='larry3d', width=90)
         print(title)
-
-        level = str()
-        while level != '1' and level != '2':
-            print('Please select difficulty level:')
-            print('1 = Easy')
-            print('2 = Medium')
-            level = input()
-
-        square = str()
-        while square != 'X' and square != 'O':
-            print('Select your square')
-            print('X')
-            print('O')
-            square = input()
-
-        """
-        Para esta práctica decidimos implementar el siguiente formato de mensajes,
-        el cual es una tupla con una cadena y un diccionario de la siguiente manera
-        
-        ('CADENA QUE INTERPRETA EL SERVIDOR','argumento1','argumento2', ... )
-        
-        A excepción de las coordenadas para tiro, las cuales se envian como un entero
-        truncado a 32 bits con signo
-        
-        Las respuestas del servidor tienen el mismo formato de mensajes,
-        por lo que para finalizar la comunicación compararemos el valor
-        de la cadena
-        """
-        command = f'GAME,{level},{square}'
-
-        """
-        Solo se pueden enviar bytes por sockets, por lo que es necesario
-        codificar la cadena con el método encode()
-        """
-        command_bytes: bytes = str(command).encode()
-
-        # Enviamos el mensaje al servidor
-        s.send(command_bytes)
-        n = 3 if int(level) == 1 else 5 #número de filas y columnas según la dificultad
+        print("I want to ...")
+        option = str()
+        while option != '1' and option != '2':
+            print('1 = Create a game room')
+            print('2 = Join a game room')
+            option = input()
+        if option == '1':
+            level = str()
+            while level != '1' and level != '2':
+                print('Please select difficulty level:')
+                print('1 = Easy')
+                print('2 = Medium')
+                level = input()
+            square = str()
+            while square != 'X' and square != 'O':
+                print('Select your square')
+                print('X')
+                print('O')
+                square = input()
+            single_player = str()
+            while single_player != '1' and single_player != '2':
+                print('Players ... ')
+                print(1)
+                print(2)
+                single_player = input()
+            """
+            Para esta práctica decidimos implementar el siguiente formato de mensajes,
+            el cual es una tupla con una cadena y un diccionario de la siguiente manera
+            
+            ('CADENA QUE INTERPRETA EL SERVIDOR','argumento1','argumento2', ... )
+            
+            A excepción de las coordenadas para tiro, las cuales se envian como un entero
+            truncado a 32 bits con signo
+            
+            Las respuestas del servidor tienen el mismo formato de mensajes,
+            por lo que para finalizar la comunicación compararemos el valor
+            de la cadena
+            """
+            command = f'GAME,{level},{square},{1 if single_player == '1' else 0}'
+            n = 3 if int(level) == 1 else 5  # número de filas y columnas según la dificultad
+            command_bytes: bytes = str(command).encode()
+            # Enviamos el mensaje al servidor
+            s.send(command_bytes)
+        else:
+            valid_code = False
+            while not valid_code:
+                print('Code ...')
+                code = int(input())
+                command = f'JOIN,{code},'
+                command_bytes: bytes = str(command).encode()
+                s.send(command_bytes)
+                squares = s.recv(1)
+                n = int.from_bytes(squares, byteorder='big', signed=True)
+                valid_code = n > 0
 
         """
         El tablero es una matriz de canedas para saber que fila y columna se va a actualizar
-        por las jugadas que haga el servidor o segundo jugador
-        
-        el codigo de abajo es equivalente a
+        por las jugadas que haga el servidor o segundo jugador el codigo de abajo es equivalente a
         board = []
         for i in range(n):
             board.append([])
             for _ in range(n):
-                board[i].append('-')
-        
+                board[i].append('-')    
         range(n) devuelve un objeto que representa una secuencia de 0 a n - 1, además es iterable
         """
         board = [[' - ' for _ in range(n)] for _ in range(n)]
-        update_board(board)
-        """
-        if square == 'X':
-            x = int(input('X: '))
-            y = int(input('Y: '))
-            s.send(str(f'{x},{y}').encode())
-        """
         while True:
             message = s.recv(12).decode().split(',') #Mensajes siempre truncados a 12 bytes
             #print(message)
             match message[0]:
+                case 'REDY':
+                    cls()
+                    update_board(board)
                 case 'MOVE':
                     x = int(message[1])
                     y = int(message[2])
                     square = message[3]
                     cls()
                     update_board(board, (x, y), square)
+                case 'WAIT':
+                    print(f' Waiting for {message[1]} ')
                 case 'TURN':
+                    print(f'Your turn {message[3]} ... ')
                     x = None
                     y = None
                     are_integers = False
@@ -155,14 +167,18 @@ if __name__ == '__main__':
                     Es importante truncar los bytes de un mensaje para que no se leea más de un mensaje
                     a la vez con el método recv
                     """
-                    assert s.send(x.to_bytes(length=32, signed=True)) == 32 #Verificar que si se envian 32 bytes al servidor
-                    assert s.send(y.to_bytes(length=32, signed=True)) == 32
+                    assert s.send(x.to_bytes(length=32, byteorder='big', signed=True)) == 32  # Verificar que si se envian 32 bytes al servidor
+                    assert s.send(y.to_bytes(length=32, byteorder='big', signed=True)) == 32
                     #s.send(f'{move[0]},{move[1]}'.encode())
                 case 'INMV':
                     cls()
                     msg = 'out of range' if message[1] == 'NORANGE' else 'taken'
                     print(f'The square is {msg}')
                     update_board(board)
+                case 'SHAC':  # Share code
+                    cls()
+                    print('Please share this code with someone to play')
+                    print(message[1])
                 case 'ENDG':
                     winner = message[1]
                     time_taken = message[2]
